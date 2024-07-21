18f115a0ddc12a0930f0eb2f9f119b190c71017e
Peter Conrad <p.conrad@proton.me>
2024-07-21 14:41:30 +0200

    Fix checkdoc issues

--- evil-neo.el
+++ evil-neo.el
@@ -24,15 +24,14 @@
 
 ;;; Commentary:
 
-;; Evil Neo mode allows you to use the Neo keyboard layout with evil-mode.
-;; Evil-Neo allows you to use evil's keybindings with the German Neo keyboard
-;; layout by remapping the hjkl keys to Neo's snrt keys. Most keybindings are
-;; simply swapped with their counterports, but a few liberties where taken when
-;; it made sense. The keys are mapped in their own minor mode, enabling the user
+;; Evil-Neo allows you to use Evil's keybindings with the German Neo keyboard
+;; layout by remapping the hjkl keys to Neo's snrt keys.  Most keybindings are
+;; simply swapped with their counterparts, but a few liberties where taken when
+;; it made sense.  The keys are mapped in their own minor mode, enabling the user
 ;; to turn it on and off as needed.
 ;; Evil-Neo aims to preserve Evil's distinction between motion, normal, and
 ;; visual states, mapping keys only in the modes they were originally defined
-;; in, where possible. The keybindings are exhaustive, with all keybindings
+;; in, where possible.  The keybindings are exhaustive, with all keybindings
 ;; defined in evil-motion-state-map, evil-normal-state-map, and
 ;; evil-visual-state-map taken into account at the time of authoring this
 ;; package.
